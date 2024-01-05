1e19142e761b3d5ca6d79ec56883ad131981b98d
ymw0407 <77202633+ymw0407@users.noreply.github.com>
2024-01-05 10:58:47 +0900

    Feat : image test setting complete
    Signed-off-by: ymw0407 <77202633+ymw0407@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -7,21 +7,6 @@ USER_FILES=$(wildcard $(USER_DIR)/*.proto)
 USER_OUT=./proto/v1/user
 USER_GRPC_OUT=./proto/v1/user
 
-ITEM_DIR=./proto/v1/item
-ITEM_FILES=$(wildcard $(ITEM_DIR)/*.proto)
-ITEM_OUT=./proto/v1/item
-ITEM_GRPC_OUT=./proto/v1/item
-
-LOCATION_DIR=./proto/v1/location
-LOCATION_FILES=$(wildcard $(LOCATION_DIR)/*.proto)
-LOCATION_OUT=./proto/v1/location
-LOCATION_GRPC_OUT=./proto/v1/location
-
-IMAGE_DIR=./proto/v1/image
-IMAGE_FILES=$(wildcard $(IMAGE_DIR)/*.proto)
-IMAGE_OUT=./proto/v1/image
-IMAGE_GRPC_OUT=./proto/v1/image
-
 # 프로토콜 버퍼 컴파일 타겟
 user: proto/v1/user/*.proto
 	$(PROTOC) -I $(USER_DIR) \
@@ -30,27 +15,3 @@ user: proto/v1/user/*.proto
 		--go-grpc_out=$(USER_GRPC_OUT) \
 		--go-grpc_opt=paths=source_relative \
 		$(USER_FILES)
-
-item: proto/v1/item/*.proto
-	$(PROTOC) -I $(ITEM_DIR) \
-		--go_out=$(ITEM_OUT) \
-		--go_opt=paths=source_relative \
-		--go-grpc_out=$(ITEM_GRPC_OUT) \
-		--go-grpc_opt=paths=source_relative \
-		$(ITEM_FILES)
-
-location: proto/v1/location/*.proto
-	$(PROTOC) -I $(LOCATION_DIR) \
-		--go_out=$(LOCATION_OUT) \
-		--go_opt=paths=source_relative \
-		--go-grpc_out=$(LOCATION_GRPC_OUT) \
-		--go-grpc_opt=paths=source_relative \
-		$(LOCATION_FILES)
-
-image: proto/v1/image/*.proto
-	$(PROTOC) -I $(IMAGE_DIR) \
-		--go_out=$(IMAGE_OUT) \
-		--go_opt=paths=source_relative \
-		--go-grpc_out=$(IMAGE_GRPC_OUT) \
-		--go-grpc_opt=paths=source_relative \
-		$(IMAGE_FILES)
--- main.go
+++ main.go
@@ -6,53 +6,62 @@ import (
 	"os"
 
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 
-	pb "test/proto/v1/image"
+	pb "test/proto"
 )
 
 func load(path string) ([]byte, error) {
-    file, e := os.Open(path)
-    if e != nil {
-        return nil, e
-    }
-    defer file.Close()
+	file, e := os.Open(path)
+	if e != nil {
+		return nil, e
+	}
+	defer file.Close()
 
-    done := make([]byte, 0)
-    buf := make([]byte, 2048)
-    for {
-        _, e := file.Read(buf)
-        if e == io.EOF {
-            break
-        }
-        if e != nil {
-            return nil, e
-        }
-        done = append(done, buf...)
-    }
-    return done, nil
+	done := make([]byte, 0)
+	buf := make([]byte, 2048)
+	for {
+		_, e := file.Read(buf)
+		if e == io.EOF {
+			break
+		}
+		if e != nil {
+			return nil, e
+		}
+		done = append(done, buf...)
+	}
+	return done, nil
 }
 
 func main() {
 	conn, e := grpc.Dial("127.0.0.1:8080", grpc.WithInsecure())
-    if e != nil {
-        return
-    }
-    defer conn.Close()
+	if e != nil {
+		return
+	}
+	defer conn.Close()
 
-    // gRPC 클라이언트 접속
-    c := pb.NewImageServiceClient(conn)
-	sp, e := load("image.jpg")
+	// gRPC 클라이언트 접속
+	c := pb.NewUserServiceClient(conn)
+	sp, e := load("image.png")
 	if e != nil {
 		println(e.Error())
 	}
-    res, e := c.ProfileImage(context.TODO(), &pb.ProfileImageRequest{
-		Uid: "2qLyPFDI2zYZ7hRTedXVsxI8waS2",
-		Image: sp,
+	res, e := c.UpdateProfile(context.TODO(), &pb.UpdateProfileRequest{
+		Uid:  "6596588ed8de6590c8ee902f",
+		Name: wrapperspb.String("윤민우"),
+		Chip: &pb.Chip{
+			SchoolName:    "국민대",
+			Grade:         "2학년",
+			InstaAccount:  "ymw0407",
+			TiktokAccount: "",
+			Mbti:          "INFJ",
+			Others:        []string{"test1", "test2"},
+		},
+		ProfileImg:  sp,
+		Description: wrapperspb.String("test Description"),
 	})
 	if e != nil {
 		println(e.Error())
 	}
-	println(res.Success)
-    println(res.Msg)
-	println(res.Url)
+	println(res.Status.Success, res.Status.ErrCode)
 }
